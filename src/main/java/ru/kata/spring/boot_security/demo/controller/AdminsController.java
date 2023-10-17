9031afabbabd072ed766574356ef8e1078726472
OlezaCoder <oleg.kopysov2019@gmail.com>
2023-10-18 01:33:48 +0400

    deleted unused imports

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminsController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminsController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.repositories.RolesRepository;
 import ru.kata.spring.boot_security.demo.service.UsersService;
 import javax.validation.Valid;
 
